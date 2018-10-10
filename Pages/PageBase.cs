4f67d79182085d7d279b2a8bd6d1b537e2bab4cf
starlying <starlying@gmail.com>
2018-10-10 16:50:38 +0800

    permissions

--- Pages/PageBase.cs
+++ Pages/PageBase.cs
@@ -31,7 +31,7 @@ namespace SS.GovPublic.Pages
 
             SiteId = Convert.ToInt32(Request.QueryString["siteId"]);
 
-            if (!Main.Instance.AdminApi.HasSitePermissions(SiteId, Main.Instance.Id))
+            if (!Main.Instance.Request.AdminPermissions.HasSitePermissions(SiteId, Main.Instance.Id))
             {
                 HttpContext.Current.Response.Write("<h1>未授权访问</h1>");
                 HttpContext.Current.Response.End();
