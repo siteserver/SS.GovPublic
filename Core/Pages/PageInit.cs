3809d57dbb6d26dc8c838209abb004fcc1e2e71b
starlying <starlying@gmail.com>
2019-05-31 10:46:50 +0800

    init

--- Core/Pages/PageInit.cs
+++ Core/Pages/PageInit.cs
@@ -23,7 +23,7 @@ namespace SS.GovPublic.Core.Pages
 
         public static string GetRedirectUrl(int siteId, string redirectUrl)
         {
-            return $"pages/{nameof(PageInit)}.aspx?siteId={siteId}&redirectUrl={HttpUtility.UrlEncode(redirectUrl)}";
+            return $"{nameof(PageInit)}.aspx?siteId={siteId}&redirectUrl={HttpUtility.UrlEncode(redirectUrl)}";
         }
 
         public void Page_Load(object sender, EventArgs e)
